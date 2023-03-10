29b00dfd1fced9f0d2dd619879a1d83b894c6434
Alyson Campos <alyson.siqueiracampos@gmail.com>
2023-03-10 07:03:54 -0300

    first commit

---
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -0,0 +1,46 @@
+package junit5tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParameterizedTests {
+
+    @ParameterizedTest(name = "Run: {index} - Value: {arguments}")
+    @ValueSource(ints = {1, 5, 6})
+    public void intValues(int theParam) {
+        System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest(name = "Run: {index} - Value: {arguments}")
+    @NullAndEmptySource
+    @ValueSource(strings = {"firstString", "secondString"})
+    public void stringValues(String theParam) {
+        System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,rogers", "captain,marvel", "bucky,barnes"})
+    public void csvSource_StringString(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,32,true", "captain,22,false", "bucky,5,true"})
+    public void csvSource_StringIntBoolen(String param1, int param2, boolean param3) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"captain america, 'steve,rogers'", "winter soldier, 'bucky,barnes'"})
+    public void csvSource_StringWithComma(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve?rogers", "bucky?barnes"}, delimiter = '?')
+    public void csv_StringWithDiffDelimiter(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+}
